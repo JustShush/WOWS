517bb93e97b3346a158a0543292a3ca6ac08a7a7
JustShush <marquesdinis.p@gmail.com>
2025-01-18 12:21:42 +0000

    ya just some prints

--- rmDuplicates.js
+++ rmDuplicates.js
@@ -1,8 +1,8 @@
 const fs = require('fs').promises;
 
 
-let removed = 0;
-let valid = 0;
+let added = 0;
+let all = 0;
 function removeDuplicates(data) {
 	// Create a Set to track unique webhooks
 	const seenWebhooks = new Set();
@@ -10,25 +10,25 @@ function removeDuplicates(data) {
 	// Filter the array to include only unique webhooks
 	return data.filter(item => {
 		if (seenWebhooks.has(item.webhook)) {
-			removed++;
-			valid++;
+			all++;
 			return false; // Exclude if the webhook is already seen
 		} else {
 			seenWebhooks.add(item.webhook); // Mark this webhook as seen
-			valid++;
+			added++;
+			all++;
 			return true; // Include the item
 		}
 	});
 }
 
 async function main() {
-	const filePath = "gwebhooks.json";
+	const filePath = "webhooks.json";
 	// Read and parse the JSON file
 	const preJson = await fs.readFile(filePath, 'utf8');
 	const webhooksJson = JSON.parse(preJson);
 	webhooksJson.gwh = removeDuplicates(webhooksJson.gwh);
 
-	console.log(`WebHooks that are valid: ${valid - removed} | Removed: ${removed}`);
+	console.log(`WebHooks that are valid: ${added} | Added: ${all - added}`);
 	await fs.writeFile(filePath, JSON.stringify(webhooksJson, null, "\t"));
 }
 
